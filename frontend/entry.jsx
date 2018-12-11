e39fd0294d501b12856f793be1ae69d6d7c932e3
Micah Jaffe <micah.jaffe95@gmail.com>
2018-12-10 21:39:22 -0800

    Add SessionApiUtil AJAX helper functions

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { signup, login, logout } from './util/session_api_util';
+
 document.addEventListener('DOMContentLoaded', () => {
   // const store = configureStore();
+
+  // TESTING
+  window.signup = signup;
+  window.login = login;
+  window.logout = logout;
+  //
   const root = document.getElementById('root');
   ReactDOM.render(
     <h1>Hello</h1>,
     root
   )
-})
+});
