ae18d38a94ebc1f5b1e76e979de037b039fbf29e
Micah Jaffe <micah.jaffe95@gmail.com>
2018-12-10 21:46:06 -0800

    Add session actions and reducer

--- frontend/actions/session_actions.js
+++ frontend/actions/session_actions.js
@@ -1,1 +1,40 @@
-//
+import * as SessionApiUtil from '../util/session_api_util';
+
+export const RECEIVE_CURRENT_USER = "RECEIVE_CURRENT_USER";
+export const LOGOUT_CURRENT_USER = "LOGOUT_CURRENT_USER";
+export const RECEIVE_SESSION_ERRORS = "RECEIVE_SESSION_ERRORS";
+
+const receiveCurrentUser = (user) => ({
+  type: RECEIVE_CURRENT_USER,
+  user
+});
+
+const logoutCurrentUser = () => ({
+  type: LOGOUT_CURRENT_USER
+});
+
+const receiveErrors = errors => ({
+  type: RECEIVE_SESSION_ERRORS,
+  errors
+});
+
+export const login = (user) => (dispatch) => (
+  SessionApiUtil.login(user).then(
+    user => dispatch(receiveCurrentUser(user)),
+    errors => dispatch(receiveErrors(errors.responseJSON))
+  )
+);
+
+export const signup = (user) => (dispatch) => (
+  SessionApiUtil.signup(user).then(
+    user => dispatch(receiveCurrentUser(user)),
+    errors => dispatch(receiveErrors(errors.responseJSON))
+  )
+);
+
+export const logout = () => (dispatch) => (
+  SessionApiUtil.logout().then(
+    () => dispatch(logoutCurrentUser()),
+    errors => dispatch(receiveErrors(errors.responseJSON))
+  )
+);
