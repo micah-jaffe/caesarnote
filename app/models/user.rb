66153387bf2bcf5b993a1f65a473a63e03dbb702
Micah Jaffe <micah.jaffe95@gmail.com>
2018-12-10 21:03:24 -0800

    Add User model and migration

---
+++ app/models/user.rb
@@ -0,0 +1,36 @@
+class User < ApplicationRecord
+  validates :username, :email, :session_token, :password_digest, presence: true
+  validates :username, :email, :session_token, uniqueness: true
+  validates :password, length: { minimum: 6, allow_nil: true }
+
+  attr_reader :password
+  before_validation :ensure_session_token
+
+  def self.generate_session_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def self.find_by_credentials(email, password)
+    user = User.find_by_email(email)
+    user && user.has_password?(password) ? user : nil
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def has_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def ensure_session_token
+    self.session_token ||= User.generate_session_token
+  end
+
+  def reset_session_token!
+    self.session_token = User.generate_session_token
+    self.save!
+    self.session_token
+  end
+end
