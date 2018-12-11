2c592280e0150454358d67459ef78716e3fa46f0
Micah Jaffe <micah.jaffe95@gmail.com>
2018-12-10 20:46:20 -0800

    Add root element to entry file

---
+++ frontend/entry.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener('DOMContentLoaded', () => {
+  // const store = configureStore();
+  const root = document.getElementById('root');
+  ReactDOM.render(
+    <Root />,
+    root
+  )
+})
