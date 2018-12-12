b4eb836637e03517caf680f19dc5975ceb66f183
Micah Jaffe <micah.jaffe95@gmail.com>
2018-12-12 09:39:02 -0800

    Add homepage nav bar styling

--- frontend/components/home/home.jsx
+++ frontend/components/home/home.jsx
@@ -1,8 +1,39 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
-  return (
-    <h1>Home</h1>
+  const linkStyle = {
+    textDecoration: 'none',
+    color: 'rgb(0, 168, 45)'
+  };
+
+  return ( 
+    <div className="home">
+      <nav className="main-nav">
+        <img className="logo" src={window.images.logo} />
+
+        <ul className="dropdown">
+          <li>Plans</li>
+          <li>Features</li>
+          <li>Help &amp; Learning</li>
+          <li>About Us</li>
+        </ul>
+
+        <ul className="signup-login">
+          <li id="signup-link">
+            <Link style={linkStyle} to="/signup">Sign up</Link>
+          </li>
+          <li>
+            or
+          </li>
+          <li id="login-btn">
+            <Link style={linkStyle} to="/login">Log in</Link>
+          </li>
+        </ul>
+
+
+      </nav>
+    </div>
   );
 };
 
