e1aedbaa40c42ce1d83d7511d71836735c8b2a2e
Micah Jaffe <micah.jaffe95@gmail.com>
2018-12-11 18:02:53 -0800

    Add styling to login form

--- frontend/components/session/session_form.jsx
+++ frontend/components/session/session_form.jsx
@@ -47,13 +47,13 @@ class SessionForm extends React.Component {
     switch (this.props.formType) {
       case 'signup':
         return (
-          <li className="disclaimer-container">
-            <label>By creating an account, you are agreeing to our <a href="#">Terms of Service</a> and <a href="#">Privacy Policy</a>.</label>
+          <li className="row disclaimer-container">
+            <p>By creating an account, you are agreeing to our <a href="#">Terms of Service</a> and <a href="#">Privacy Policy</a>.</p>
           </li>
         );
       case 'login':
         return (
-          <li className="remember-me-row">
+          <li className="row remember-me-row">
             <input id="remember-me" type="checkbox" />
             <label htmlFor="remember-me">Remember me for 30 days</label>
           </li>
@@ -63,57 +63,64 @@ class SessionForm extends React.Component {
 
   render() {    
     return (
-      <div className="login-form-container">
-        <form className="login-form-box" onSubmit={this.handleSubmit}>
-          <ol>
-            <li className="row">
-              <div>
-                <button onClick={this.demoLogin}>Continue as demo user</button>
-              </div>
-            </li>
+      <div className="login-page">
+      
+        <div className="login-form-container">
 
-            <li className="row horizontal-row">
-              <div>
+          <div className="login-form-header">
+            <img className="logo" src={window.images.logo} />
+            <h2 className="app-name">Notify</h2>
+            <h4 id="tagline">Remember everything important.</h4>
+          </div>
+          
+          <form className="login-form-box" onSubmit={this.handleSubmit}>
+            <ol>
+              <li className="row">
+                <button id="demo-login-btn" onClick={this.demoLogin}>Continue as demo user</button>
+              </li>
+
+              <li className="row horizontal-row">
+                <div></div>
                 <p>or</p>
-              </div>
-            </li>
+                <div></div>
+              </li>
 
-            <li className="row">
-              <div>
+              <li className="row">
                 <input 
+                  className="login-input"
                   type="email" 
                   onChange={this.handleInput('email')} 
                   value={this.state.email}
                 />
-              </div>
-            </li>
+              </li>
 
-            <li className="row">
-              <div>
+              <li className="row">
                 <input 
+                  className="login-input"
                   type="password" 
                   onChange={this.handleInput('password')} 
                   value={this.state.password} 
                 />
+              </li>
+
+              <li>
+                <input id="continue-btn" type="submit" value="Continue" />
+              </li>
+
+              {this.renderlastItem()}
+            </ol>
+
+            <div className="switch-session-action-container">
+              <p>{this.props.switchText}</p>
+              <div onClick={this.props.clearErrors}>
+                {this.props.navLink}
               </div>
-            </li>
-
-            <li>
-              <input type="submit" value="Continue" />
-            </li>
-
-            {this.renderlastItem()}
-          </ol>
-
-          <div className="switch-session-action-container">
-            <p>{this.props.switchText}</p>
-            <div onClick={this.props.clearErrors}>
-              {this.props.navLink}
             </div>
-          </div>
-          
-          {this.renderErrors()}
-        </form>
+            
+            {this.renderErrors()}
+          </form>
+        </div>
+
       </div>
     )
   }
