4d649c9a6e16cdd5d066cc62f4d5ffe08c78d424
Micah Jaffe <micah.jaffe95@gmail.com>
2018-12-12 17:54:26 -0800

    Style user profile

--- frontend/components/main/sidebar/user_profile.jsx
+++ frontend/components/main/sidebar/user_profile.jsx
@@ -2,13 +2,19 @@ import React from 'react';
 
 const UserProfile = props => {
   return (
-    <div className="user-profile">
-      <span>
-        <img className="avatar" src={window.images.logo} />
-      </span>
+    <div className="user-profile-container">
+      <span className="user-profile">
+        <span>
+          <img className="avatar" src={window.images.avatar} />
+        </span>
 
-      <span>
-        {props.currentUser.username}
+        <span>
+          {props.currentUser.username}
+        </span>
+
+        <span>
+          <svg xmlns="http://www.w3.org/2000/svg" width="8" height="8" viewBox="0 0 8 8" className="profile-svg"><path fill="none" d="M7 2L4 5 1 2"></path></svg>
+        </span>
       </span>
 
     </div>
