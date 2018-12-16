9bce402ae74dbf8711b18c2621f996e90f960323
Micah Jaffe <micah.jaffe95@gmail.com>
2018-12-16 14:38:46 -0800

    Style homepage header

--- frontend/components/home/home.jsx
+++ frontend/components/home/home.jsx
@@ -4,15 +4,23 @@ import { Link } from 'react-router-dom';
 const Home = () => {
   return ( 
     <div className="home">
-      <nav className="main-nav">
-        <img className="logo" src={window.images.logo} />
 
-        <ul className="nav-links">
-          <li>Plans</li>
-          <li>Features</li>
-          <li>Help &amp; Learning</li>
-          <li>About Us</li>
-        </ul>
+      <nav className="main-nav">
+        <div className="left-nav-wrapper">
+          <div className="logo-with-text align-middle">
+            <a href="/">
+              <img className="logo" src={window.images.logo} />
+              <h1>Caesarnote</h1>
+            </a>
+          </div>
+
+          <ul className="nav-links">
+            <li>Plans</li>
+            <li>Features</li>
+            <li>Help &amp; Learning</li>
+            <li>About Us</li>
+          </ul>
+        </div>
 
         <ul className="signup-login">
           <li>
@@ -25,11 +33,18 @@ const Home = () => {
             <Link id="login-btn" to="/login">Log in</Link>
           </li>
         </ul>
-
-
       </nav>
+
+      <div className="body">
+        <div className="hero-image">
+
+        </div>
+      </div>
+
+
     </div>
   );
 };
 
 export default Home;
+
