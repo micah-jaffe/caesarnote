9fd2313f296ddd9781723cc4174b8879cb5cab39
Micah Jaffe <micah.jaffe95@gmail.com>
2018-12-18 20:24:43 -0800

    Add TagsIndex component

---
+++ frontend/components/main/tags/tags_index.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import TagsIndexItem from './tags_index_item';
+
+class TagsIndex extends React.Component {
+  componentDidMount() {
+    this.props.fetchTags();
+  }
+
+  sortTags() {
+    this.props.tags.sort((a, b) => a.name < b.name ? 1 : -1);
+  }
+
+  renderTagsIndexHeader() {
+    return (
+      <div className="tags-header">
+        <h1>Tags</h1>
+      </div>
+    );
+  }
+
+  renderTagsIndexItems() {
+    this.sortTags();
+
+    return (
+      <div className="tags-index-items">
+        {this.props.tags.map(tag => <TagsIndexItem key={tag.id} tag={tag} />)}
+      </div>
+    );
+  }
+
+  render() {
+    return (
+      <div className="tags-index-wrapper">
+        {this.renderTagsIndexHeader()}
+        {this.renderTagsIndexItems()}
+      </div>
+    );
+  }
+};
+
+export default TagsIndex;
---
+++ frontend/components/main/tags/tags_index_container.jsx
@@ -0,0 +1,23 @@
+import { connect } from 'react-redux';
+import TagsIndex from './tags_index';
+import { fetchTags, deleteTag } from '../../../actions/notebook_actions';
+import {
+  openModal,
+  closeModal
+} from '../../../actions/modal_actions';
+
+const mapStateToProps = (state) => ({
+  tags: Object.values(state.entities.tags)
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  fetchTags: () => dispatch(fetchTags()),
+  deleteTag: (id) => dispatch(deleteTag(id)),
+  openModal: (modal) => dispatch(openModal(modal)),
+  closeModal: () => dispatch(closeModal())
+});
+
+export default connect(
+  mapStateToProps, 
+  mapDispatchToProps
+  )(TagsIndex);
