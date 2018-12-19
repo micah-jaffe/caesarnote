d1ac0ed6f6e5d0827338fad9287236914b20fc86
Micah Jaffe <micah.jaffe95@gmail.com>
2018-12-18 21:06:55 -0800

    Add tag alphabetizing functionality

--- frontend/components/main/tags/tags_index.jsx
+++ frontend/components/main/tags/tags_index.jsx
@@ -21,11 +21,27 @@ class TagsIndex extends React.Component {
   renderTagsIndexItems() {
     this.sortTags();
 
-    return (
-      <div className="tags-index-items-wrapper">
-        {this.props.tags.map(tag => <TagsIndexItem key={tag.id} tag={tag} />)}
-      </div>
-    );
+    const partitionedTags = [];
+
+    for (let i = 33; i <= 126; i++) {
+      const filteredTags = this.props.tags.filter(tag => tag.name.charAt(0) === String.fromCharCode(i));
+
+      if (filteredTags.length > 0) {
+        const block = (
+          <div key={`block-${i}`} className="tags-index-block">
+            <h2>{String.fromCharCode(i)}</h2>
+            <div className="tags-block-items">
+              {filteredTags.map(tag => <TagsIndexItem key={tag.id} tag={tag} />)}
+            </div>
+          </div>
+        );
+
+        partitionedTags.push(block)
+
+      }
+    };
+
+    return partitionedTags;
   }
 
   render() {
