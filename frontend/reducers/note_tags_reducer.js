8bfba2880d4a7567fd36671b1a37e9a1caac734d
Micah Jaffe <micah.jaffe95@gmail.com>
2018-12-18 18:29:21 -0800

    Debug tag show

--- frontend/reducers/note_tags_reducer.js
+++ frontend/reducers/note_tags_reducer.js
@@ -1,27 +1,27 @@
-import {
-  RECEIVE_NOTE_TAGS,
-  RECEIVE_NOTE_TAG,
-  REMOVE_NOTE_TAG
-} from '../actions/note_tag_actions';
+// import {
+//   RECEIVE_NOTE_TAGS,
+//   RECEIVE_NOTE_TAG,
+//   REMOVE_NOTE_TAG
+// } from '../actions/note_tag_actions';
 
 import {
-  RECEIVE_TAGS
+  RECEIVE_TAGS,
+  RECEIVE_TAG,
+  REMOVE_TAG
 } from '../actions/tag_actions';
 
 const noteTagsReducer = (state = {}, action) => {
   Object.freeze(state);
 
   switch (action.type) {
-    case RECEIVE_NOTE_TAGS:
-      return action.payload.note_tags;
     case RECEIVE_TAGS:
       return action.payload.note_tags;
-    case RECEIVE_NOTE_TAG:
+    case RECEIVE_TAG:
       return Object.assign({}, state, action.payload.note_tags);
-    case REMOVE_NOTE_TAG:
-      const newState = Object.assign({}, state);
-      delete newState[action.noteTagId]
-      return newState;
+    // case REMOVE_TAG:
+    //   const newState = Object.assign({}, state);
+    //   delete newState[action.noteTagId]
+    //   return newState;
     default:
       return state;
   }
