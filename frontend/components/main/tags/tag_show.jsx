c0affcffebc25c2ccd850c8108125f24cf442acb
Micah Jaffe <micah.jaffe95@gmail.com>
2018-12-19 07:46:01 -0800

    Fix tag selection highlighting

--- frontend/components/main/tags/tag_show.jsx
+++ frontend/components/main/tags/tag_show.jsx
@@ -19,6 +19,9 @@ class TagShow extends React.Component {
 
     this.showDropdown = this.showDropdown.bind(this);
     this.hideDropdown = this.hideDropdown.bind(this);
+    this.highlightTag = this.highlightTag.bind(this);
+    this.handleDropdownClick = this.handleDropdownClick.bind(this);
+    this.clearTags = this.clearTags.bind(this);
   }
 
   showDropdown() {
@@ -29,6 +32,23 @@ class TagShow extends React.Component {
     this.setState({ dropdown: false })
   }
 
+  highlightTag() {
+    this.clearTags();
+    document.getElementById(`tag-${this.props.tag.id}`).classList.add('selected-tag');
+  }
+
+  clearTags() {
+    const otherTags = document.getElementsByClassName('tag-show');
+    for (let i = 0; i < otherTags.length; i++) {
+      otherTags[i].classList.remove('selected-tag');
+    }
+  }
+
+  handleDropdownClick() {
+    this.showDropdown();
+    this.highlightTag();
+  }
+
   render() {
     const dropdownItems = {
       "Remove": () => console.log('hello'),
@@ -36,13 +56,19 @@ class TagShow extends React.Component {
     };
 
     return (
-      <div className="tag-show align-middle" tabIndex="0">
+      <div 
+        id={`tag-${this.props.tag.id}`} 
+        className="tag-show align-middle" 
+        tabIndex="0"
+        onClick={this.highlightTag}
+        onBlur={this.clearTags}
+      >
         <Dropdown visible={this.state.dropdown} classname="tag-show-dropdown" items={dropdownItems} />
         <p>{this.props.tag.name}</p>
         <span 
           className="dropdown-icon"
           tabIndex="0"
-          onFocus={this.showDropdown}
+          onFocus={this.handleDropdownClick}
           onBlur={this.hideDropdown}
         >
          <i className="fa fa-angle-down"></i> 
