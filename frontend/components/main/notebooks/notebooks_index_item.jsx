a8f9c3c6aee00ec65226ed45ac941aebacd4706b
Micah Jaffe <micah.jaffe95@gmail.com>
2018-12-19 21:19:58 -0800

    Remove notebook count errors

--- frontend/components/main/notebooks/notebooks_index_item.jsx
+++ frontend/components/main/notebooks/notebooks_index_item.jsx
@@ -31,8 +31,9 @@ class NotebooksIndexItem extends React.Component {
   }
 
   render() {
-    const { notebookId, name, notes } = this.props.notebook;
-    // const numNotes = Object.keys(notes).length > 0 ? this.props.selectNotebookNotes(notebookId, notes).length : ''
+    const { notebookId, name } = this.props.notebook;
+    const { notes, selectNotebookNotes } = this.props;
+    const numNotes = Object.keys(notes).length > 0 ? selectNotebookNotes(notebookId, notes).length : '';
 
     const dropdownItems = {
       "Move to...": this.openPaywall,
@@ -50,7 +51,7 @@ class NotebooksIndexItem extends React.Component {
           <svg width="6" height="9" viewBox="2 240 6 9" xmlns="http://www.w3.org/2000/svg" className="svg notebook-index-icon"><path fill="#9B9B9B" d="M2 240l6 4.5-6 4.5z"></path></svg>
           <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" className="svg notebook-index-icon" viewBox="0 0 24 24"><path fill="#313131" d="M16 8.33c0-.18-.22-.33-.5-.33h-4c-.28 0-.5.15-.5.33v1.34c0 .18.22.33.5.33h4c.28 0 .5-.15.5-.33zM18 6v11a2 2 0 0 1-2 2H9V4h7a2 2 0 0 1 2 2zM6 4h2v15H6z"></path></svg>
           <Link className="notebook-link" to={`/main/notebooks/${notebookId}`}>{name}</Link>
-          <span className="notebook-notes-count">({2})</span>
+          <span className="notebook-notes-count">({numNotes})</span>
         </div>
 
         <ul className="align-middle">
