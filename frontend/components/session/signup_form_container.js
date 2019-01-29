1e8c428c89d42d84782fa7b3a873cbaac4b07ebc
Micah Jaffe <micah.jaffe95@gmail.com>
2019-01-28 19:30:33 -0800

    Debug signup form issue

--- frontend/components/session/signup_form_container.js
+++ frontend/components/session/signup_form_container.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { login, clearErrors } from '../../actions/session_actions';
+import { signup, login, clearErrors } from '../../actions/session_actions';
 import SessionForm from './session_form';
 
 const mapStateToProps = ({ errors }) => ({
